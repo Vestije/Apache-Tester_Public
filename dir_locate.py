cba0332333bd8db58ecd4ef93d5162d89ed58332
Kali <kali@kali>
2021-04-07 02:40:44 -0400

    added comments to code

--- dir_locate.py
+++ dir_locate.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+
 import os
 import os.path
 
-for dirpath, dirnames, filenames in os.walk("/"):
-    for filename in [f for f in filenames if f.endswith("apache2.conf" or "httdp.conf")]:
-        print(os.path.join(dirpath, filename))
-    for filename in [f for f in filenames if f.endswith("security.conf")]:
-        print(os.path.join(dirpath, filename))
+def find_dir():
+    found_directories = "" # ----> blank variable to add found directories too
+    for dirpath, dirnames, filenames in os.walk("/"): # ----> use os.walk to look through each directory
+        for filename in [f for f in filenames if f.endswith("apache2.conf" or "httdp.conf")]: # ----> search through directories to find specific file names
+            found_directories += os.path.join(dirpath, filename) + '\n' # ----> add found files to a string
+        for filename in [f for f in filenames if f.endswith("security.conf")]: # ----> search through directories to find specific file names
+            found_directories += os.path.join(dirpath, filename) + '\n' # ----> add found files to a string
+    return (found_directories) # ----> return the finalized string with all directories found on the machine that meet the criteria
 
+
+if __name__ == "__main__":
+    print(find_dir())
 
--- dir_search.py
+++ dir_search.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+import re
 from dir_locate import find_dir
 
 
 
 def search():
-    directories = find_dir().split('\n')
+    directories = find_dir().split('\n') # ----> run find_dir() to get a list of active directories, split them into a list at each new line
     
-    for each_dir in directories:
-        if 'apache2.conf' in each_dir:
-            return(f'{each_dir} has apache2.conf')
+    for each_dir in directories:       
+        searched_terms = '' # ----> blank variable to add searched lines 
+        if 'apache2.conf' in each_dir: # ----> if the string is part of the directory name: run
+            conf_file = open(each_dir, "r") # ----> opens the directory and reads ("r") the file
+            for line in conf_file: # ----> read each line in the file
+                if re.search("main Apache", line):# ----> if searched term (ex. 'main Apache') in the line: return the line
+                    #searched_terms += line + '\n' # ----> add all searched lines to a list
+        #return searched_terms # ----> list of searched data from the file
+                    return(line) # ----> for testing, remove for above lines
+
+        elif 'conf-enable/security.conf' in each_dir: # ----> if the string is part of the directory name: run
+            conf_file = open(each_dir, "r") # ----> opens the directory and reads ("r") the file
+            for line in conf_file: # ----> read each line in the file
+                if re.search("main Apache", line): # ----> if searched term (ex. 'main Apache') in the line: return the line
+                    #searched_terms += line + '\n' # ----> add all searched lines to a list
+        #return searched_terms # ----> list of searched data from the file
+                    return(line) # ----> for testing, remove for above lines
 
 print(search())
 
