276be51690de4f4d1ae6568932b1ed32e1b3e6f5
j1gawatt <72881361+j1gawatt@users.noreply.github.com>
2021-04-08 02:28:45 -0400

    Add files via upload

---
+++ Print_Report.py
@@ -0,0 +1,24 @@
+def print_list(theList):
+    for line in theList: #Loop through the list
+        print('{: ^100}'.format(line)) #Print each line
+
+def report(correct, incorrect):
+    #Print the settings that are set to recommended settings
+    print('{: ^100}'.format('The following settings are set to the recommended setting:'))
+    print('\n')
+    if not correct: #if the list is empty
+        print('{: ^100}'.format('--NONE--\n')) #Print no settings are set to recommended settings
+
+    else: #Print the list
+        print_list(correct) #Print Correct list
+        print('\n'*2) #Print a new line after all the lines in the list
+
+    #Print the settings that are not set to the recommended setting
+    print('{: ^100}'.format('The following settings are not set to the recommended setting:'))
+    print('\n')
+    if not incorrect: #If the list is empty
+        print('{: ^100}'.format('--NONE--\n')) #Print no settings are set incorrectly.
+    else: #Print the list
+        print_list(incorrect) #Print Incorrect list
+        print('\n'*5)
+
