f728b814b3364c3208477943d503fc35b2b2ce17
j1gawatt <72881361+j1gawatt@users.noreply.github.com>
2021-04-08 02:24:56 -0400

    Update apache_final_script.py

--- apache_final_script.py
+++ apache_final_script.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
 
+from os import system
+from Get_Recommendations import get_settings
+from Print_Report import report
+from dir_search import search
+
 from apache_version import get_ver
 from dir_locate import find_dir
 from dir_search import search
 
 def final_output():
-    print('╔═╗╔═╗╔═╗╔═╗╦ ╦╔═╗   ╔╦╗╔═╗╔═╗╔╦╗╔═╗╦═╗\n╠═╣╠═╝╠═╣║  ╠═╣║╣     ║ ║╣ ╚═╗ ║ ║╣ ╠╦╝\n╩ ╩╩  ╩ ╩╚═╝╩ ╩╚═╝────╩ ╚═╝╚═╝ ╩ ╚═╝╩╚═')
-    print(f'\n{get_ver()}')
-    print(f'\nYour files are located in:\n{find_dir()}')
+    system('clear') #Clear the screen
+    print('*'*100)	
+    print('{:*^100}'.format(' ╔═╗╔═╗╔═╗╔═╗╦ ╦╔═╗   ╔╦╗╔═╗╔═╗╔╦╗╔═╗╦═╗ '))
+    print('{:*^100}'.format(' ╠═╣╠═╝╠═╣║  ╠═╣║╣     ║ ║╣ ╚═╗ ║ ║╣ ╠╦╝ '))
+    print('{:*^100}'.format(' ╩ ╩╩  ╩ ╩╚═╝╩ ╩╚═╝    ╩ ╚═╝╚═╝ ╩ ╚═╝╩╚═ '))
+  
+    #*************************************************************************
+    #**** The following 3 lines just print the Apache Tester ASCII art in ****
+    #**** the center of the terminal console window.  Remove the # before ****
+    #**** each line to print them and comment out the three above instead ****
+    #*************************************************************************
+
+    #print('╔═╗╔═╗╔═╗╔═╗╦ ╦╔═╗   ╔╦╗╔═╗╔═╗╔╦╗╔═╗╦═╗'.center(shutil.get_terminal_size().columns))
+    #print('╠═╣╠═╝╠═╣║  ╠═╣║╣     ║ ║╣ ╚═╗ ║ ║╣ ╠╦╝'.center(shutil.get_terminal_size().columns))
+    #print('╩ ╩╩  ╩ ╩╚═╝╩ ╩╚═╝    ╩ ╚═╝╚═╝ ╩ ╚═╝╩╚═'.center(shutil.get_terminal_size().columns))
+    print('*'*100)
+    print('\n'* 2)
+    
+    #Declare variables 
+    settings = get_settings() #Gets the recommended settings from the Recommended_Settings.txt file
+    correct_settings = [] #List to hold all the settings that are set to the recommended setting
+    incorrect_settings = [] #List to hold all the settings that are not set to the recommended setting
+
+    #Check Settings and add to appropiate list
+    search(settings, correct_settings, incorrect_settings)
+    
+    #Print a report
+    report(correct_settings, incorrect_settings)
+    
+    #print(f'\n{get_ver()}')
+    #print(f'\nYour files are located in:\n{find_dir()}')
     ### no code yet for search() ###
 
 
