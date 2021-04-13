bda86221fdf8e7035d82a4ce8f090c40acace41e
hoangsim <hoangsim@gmail.com>
2021-04-12 23:55:10 -0700

    Update backup.py
    changed security_backup.comf to security_backup.conf

--- backup.py
+++ backup.py
@@ -7,7 +7,7 @@ def conf_backup():
     current_dir = os.path.dirname(os.path.realpath(__file__)) #current directory path
     dirlist = find_dir().splitlines() #list from output of dir_locate
     copyfile(dirlist[0],current_dir + '/apache2_backup.conf') #copy of apache2.conf
-    copyfile(dirlist[1],current_dir + '/security_backup.comf') #copy of security.conf
+    copyfile(dirlist[1],current_dir + '/security_backup.conf') #copy of security.conf
 
 if __name__ == "__main__":
     conf_backup()
