1c5f9980b10b6fb962e42040f193074ff466b805
hoangsim <hoangsim@gmail.com>
2021-04-14 22:03:53 -0700

    Update backup.py
    added a for loop to just get the "/conf-enabled/security_backup.conf"

--- backup.py
+++ backup.py
@@ -4,13 +4,15 @@ from shutil import copyfile
 from dir_locate import find_dir
 
 def conf_backup():
-    current_dir = os.path.dirname(os.path.realpath(__file__)) #current directory path
-    dirlist = find_dir().splitlines() #list from output of dir_locate
+    current_dir = os.path.dirname(os.path.realpath(__file__)) #current directory path of backup.py
+    dirlist = find_dir().splitlines() #stores output from dir_locate.py to a list
     if dirlist[0].endswith('apache2.conf'):
         copyfile(dirlist[0],current_dir + '/apache2_backup.conf') #copy of apache2.conf
     elif dirlist[0].endswith('httpd.conf'):
         copyfile(dirlist[0],current_dir + '/httpd_backup.conf') #copy of httpd.conf
-    copyfile(dirlist[1],current_dir + '/security_backup.conf') #copy of security.conf
+    for item in dirlist:
+        if item.endswith('/conf-enabled/security.conf'):
+            copyfile(item,current_dir + '/security_backup.conf') #copy of security.conf
 
 if __name__ == "__main__":
     conf_backup()
