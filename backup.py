95ef738bbed894d7cc13a70f27eb3e69568d6964
hoangsim <hoangsim@gmail.com>
2021-04-14 19:16:22 -0700

    Update backup.py
    added if/elif for apache2.conf vs httpd.conf

--- backup.py
+++ backup.py
@@ -6,7 +6,10 @@ from dir_locate import find_dir
 def conf_backup():
     current_dir = os.path.dirname(os.path.realpath(__file__)) #current directory path
     dirlist = find_dir().splitlines() #list from output of dir_locate
-    copyfile(dirlist[0],current_dir + '/apache2_backup.conf') #copy of apache2.conf
+    if dirlist[0].endswith('apache2.conf'):
+        copyfile(dirlist[0],current_dir + '/apache2_backup.conf') #copy of apache2.conf
+    elif dirlist[0].endswith('httpd.conf'):
+        copyfile(dirlist[0],current_dir + '/httpd_backup.conf') #copy of httpd.conf
     copyfile(dirlist[1],current_dir + '/security_backup.conf') #copy of security.conf
 
 if __name__ == "__main__":
