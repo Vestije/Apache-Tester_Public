da2a6821f6e7e82a476c032b8a07d50d82d2c93d
hoangsim <hoangsim@gmail.com>
2021-04-17 12:41:42 -0700

    Update backup.py
    1. Added 2 lines to generate a backup folder
    2. Added /backup to filepaths

--- src/new_version/backup.py
+++ src/new_version/backup.py
@@ -7,22 +7,24 @@ from dir_locate import find_dir
 def conf_backup():
     current_dir = os.path.dirname(os.path.realpath(__file__)) #current directory path of backup.py    
     dirlist = find_dir().splitlines() #stores output from dir_locate.py to a list
+    if os.path.isdir(current_dir + '/backup') == False: #checks if there is a backup folder
+        os.mkdir(current_dir + '/backup') #makes a backup folder
     if dirlist[0].endswith('apache2.conf'):
-        if path.exists( current_dir + '/apache2.original.back') == False: #checks if apache2.original.back exists
-            copyfile(dirlist[0],current_dir + '/apache2.original.back') #copy of apache2.conf
-        elif path.exists( current_dir + '/apache2.original.back') == True: #checks if apache2.original.back exists
-            copyfile(dirlist[0],current_dir + '/apache2.updated.back') #copy of apache2.conf
+        if path.exists( current_dir + '/backup/apache2.original.back') == False: #checks if apache2.original.back exists
+            copyfile(dirlist[0],current_dir + '/backup/apache2.original.back') #copy of apache2.conf
+        elif path.exists( current_dir + '/backup/apache2.original.back') == True: #checks if apache2.original.back exists
+            copyfile(dirlist[0],current_dir + '/backup/apache2.updated.back') #copy of apache2.conf
     elif dirlist[0].endswith('httpd.conf'):
-        if path.exists( current_dir + '/httpd.original.back') == False: #checks if httpd.original.back exists
-            copyfile(dirlist[0],current_dir + '/httpd.original.back') #copy of httpd.conf
-        elif path.exists( current_dir + '/httpd.original.back') == True: #checks if httpd.original.back exists
-            copyfile(dirlist[0],current_dir + '/httpd.updated.back') #copy of httpd.conf
+        if path.exists( current_dir + '/backup/httpd.original.back') == False: #checks if httpd.original.back exists
+            copyfile(dirlist[0],current_dir + '/backup/httpd.original.back') #copy of httpd.conf
+        elif path.exists( current_dir + '/backup/httpd.original.back') == True: #checks if httpd.original.back exists
+            copyfile(dirlist[0],current_dir + '/backup/httpd.updated.back') #copy of httpd.conf
     for item in dirlist:
         if item.endswith('/conf-enabled/security.conf'):
-            if path.exists( current_dir + '/security.original.back') == False:  #checks if security.original.back exists
-                copyfile(item,current_dir + '/security.original.back') #copy of security.conf
-            elif path.exists( current_dir + '/security.original.back') == True:  #checks if security.original.back exists
-                copyfile(item,current_dir + '/security.updated.back') #copy of security.conf
+            if path.exists( current_dir + '/backup/security.original.back') == False:  #checks if security.original.back exists
+                copyfile(item,current_dir + '/backup/security.original.back') #copy of security.conf
+            elif path.exists( current_dir + '/backup/security.original.back') == True:  #checks if security.original.back exists
+                copyfile(item,current_dir + '/backup/security.updated.back') #copy of security.conf
 
 if __name__ == "__main__":
     conf_backup()
